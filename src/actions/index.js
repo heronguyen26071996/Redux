f6074ab3b6de9596440050d5b51f797b8175f5f2
Hero <heronguyen26071996@gmail.com>
2020-03-19 10:31:38 +0700

    commit react-redux

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+
+export const addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    payload:{
+      text,
+      completed: false
+    },
+  }
+};
+export const toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+};
+export const setVisibleFilter = (filter) => {
+  return {
+    type : 'VISIBLE_FILTER',
+    filter
+  }
+};
